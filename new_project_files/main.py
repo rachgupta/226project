00e68c03118cbbed9621dce75b627c236f15cfd0
Rachna Gupta <rachgupta200@gmail.com>
2024-04-14 23:18:11 -0400

    cleaning up unusable files

--- new_project_files/main.py
+++
@@ -1,5 +0,0 @@
-from SRI_IP.src.model import solve_SRI, OptimalityCriteria
-from SRI_IP.src.utils import read_instance
-from SRI_IP.src.feasibility_checker import check_feasibility
-
-print(solve_SRI("random_5each.txt", OptimalityCriteria.EGALITARIAN))
