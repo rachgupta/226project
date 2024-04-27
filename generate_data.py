806792c4c108844d727b7247d931e2653ea3e63a
Rachna Gupta <rachgupta200@gmail.com>
2024-04-27 12:46:35 -0400

    added in-line arguments

--- generate_data.py
+++ generate_data.py
@@ -1,62 +1,63 @@
 import random
 import json
+import sys
+def generate_data(num, filename):
 
+    #IDs start here
+    #0-19
+    num_bi_men = num
+    num_gay_men = num
+    num_straight_men = num
+    num_bi_women = num
+    num_gay_women = num
+    num_straight_women = num
 
-#IDs start here
-#0-19
-num_bi_men = 5
-#20-39
-num_gay_men = 5
-#40-59
-num_straight_men = 5
-#60-79
-num_bi_women = 5
-#80-99
-num_gay_women = 5
-#100-119
-num_straight_women = 5
+    total_participants = num_bi_men +num_gay_men+num_straight_men+num_bi_women+num_gay_women+num_straight_women
 
-total_participants = num_bi_men +num_gay_men+num_straight_men+num_bi_women+num_gay_women+num_straight_women
+    all_preferences = {}
+    for i in range(1, total_participants):
+        #bi men
+        if(i < num_bi_men + 1):
+            range_beginning = 1
+            range_end = total_participants + 1
+        #gay men
+        elif(0 <= (i - num_bi_men - 1)  < num_gay_men):
+            range_beginning = 1
+            range_end = num_bi_men + num_gay_men + num_straight_men + 1
+        #straight men
+        elif(0 <= (i - num_bi_men - num_gay_men - 1) < num_straight_men):
+            range_beginning = num_bi_men + num_gay_men + num_straight_men + 1
+            range_end = total_participants + 1
+        #bi women
+        elif(0 <= (i - num_bi_men - num_gay_men - num_straight_men - 1) < num_bi_women):
+            range_beginning = 1
+            range_end = total_participants + 1
+        #straight women
+        elif(0 <= (i - num_bi_men - num_gay_men - num_straight_men - num_bi_women - 1) < num_gay_women):
+            range_beginning = 1
+            range_end = num_bi_men + num_gay_men + num_straight_men + 1
+        #gay women
+        elif(i < total_participants + 1):
+            range_beginning = num_bi_men + num_gay_men + num_straight_men + 1
+            range_end = total_participants + 1
+        
+        preferences = list(range(range_beginning, range_end))
+        random.shuffle(preferences)
+        if i in preferences:
+            preferences.remove(i)
+        all_preferences[i] = preferences
+        
+    with open(filename, "w") as file:
+        # Iterate over the dictionary items
+        file.write(str(total_participants) + '\n')
+        for key, value in all_preferences.items():
+            # Convert the array to a space-separated string
+            line = ' '.join(map(str, value))
+            # Write the formatted string to the file
+            file.write(line + '\n')
 
-all_preferences = {}
-for i in range(1, total_participants):
-    #bi men
-    if(i < num_bi_men + 1):
-        range_beginning = 1
-        range_end = total_participants + 1
-    #gay men
-    elif(0 <= (i - num_bi_men - 1)  < num_gay_men):
-        range_beginning = 1
-        range_end = num_bi_men + num_gay_men + num_straight_men + 1
-    #straight men
-    elif(0 <= (i - num_bi_men - num_gay_men - 1) < num_straight_men):
-        range_beginning = num_bi_men + num_gay_men + num_straight_men + 1
-        range_end = total_participants + 1
-    #bi women
-    elif(0 <= (i - num_bi_men - num_gay_men - num_straight_men - 1) < num_bi_women):
-        range_beginning = 1
-        range_end = total_participants + 1
-    #straight women
-    elif(0 <= (i - num_bi_men - num_gay_men - num_straight_men - num_bi_women - 1) < num_gay_women):
-        range_beginning = 1
-        range_end = num_bi_men + num_gay_men + num_straight_men + 1
-    #gay women
-    elif(i < total_participants + 1):
-        range_beginning = num_bi_men + num_gay_men + num_straight_men + 1
-        range_end = total_participants + 1
-      
-    preferences = list(range(range_beginning, range_end))
-    random.shuffle(preferences)
-    if i in preferences:
-        preferences.remove(i)
-    all_preferences[i] = preferences
-print(all_preferences)
-    
-with open("random_5each.txt", "w") as file:
-    # Iterate over the dictionary items
-    file.write(str(total_participants) + '\n')
-    for key, value in all_preferences.items():
-        # Convert the array to a space-separated string
-        line = ' '.join(map(str, value))
-        # Write the formatted string to the file
-        file.write(line + '\n')
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python generate_data.py <number> <filename>")
+    else:
+        generate_data(int(sys.argv[1]), sys.argv[2])
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from SRI_IP.src.model import solve_SRI, OptimalityCriteria
 from SRI_IP.src.utils import read_instance
+import sys
 
-print(solve_SRI("random_5each.txt", OptimalityCriteria.EGALITARIAN))
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python main.py <input file> <type> <output file>")
+        return
+    type = sys.argv[2]
+    #    NONE = 0
+    # EGALITARIAN = 1
+    # FIRST_CHOICE_MAXIMAL = 2
+    # RANK_MAXIMAL = 3
+    # GENEROUS = 4
+    # ALMOST_STABLE = 5
+    pairings = solve_SRI(sys.argv[1], type)
+    with open(sys.argv[3], 'w') as f:
+        f.write(str(pairings))
+    
+    
+
+if __name__ == "__main__":
+    main()
